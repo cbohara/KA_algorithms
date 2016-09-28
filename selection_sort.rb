13241fb6bf0a5a4cc1346181dcfec61e102f91ed
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-09-28 12:55:09 -0700

    working through subfunctions

--- selection_sort.rb
+++ selection_sort.rb
@@ -7,5 +7,23 @@ def swap(array, first_index, second_index)
 	array[second_index] = temp
 end
 
-test_array = [7, 9, 4]
-swap(test_array, 0, 1)
+# test_array = [7, 9, 4]
+# swap(test_array, 0, 1)
+
+# find index of minimum value in the subarray (from the start_index to the end)
+def index_of_minimum(array, start_index)
+  min_value = array[start_index]
+  min_index = start_index
+  n = array.length - 1
+
+  for i in start_index..n do
+    if min_value > array[i]
+      min_value = array[i]
+      min_index = i
+    end
+  end
+  return min_index
+end
+
+# array = [18, 6, 66, 44, 9, 22, 14]
+# index_of_minimum(array, 2)
