3e0494801a0c9ba4fb6f6d651d50e4884c76a3c0
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-09-30 12:23:13 -0700

    simply spec

--- recursion/spec/factorial_spec.rb
+++ recursion/spec/factorial_spec.rb
@@ -2,12 +2,10 @@ require 'factorial'
 
 RSpec.describe 'factorial' do
   it 'should calculate the factorial of n' do
-    n = 5
-    expect(factorial(n)).to eq 120
+    expect(factorial(5)).to eq 120
   end
 
   it 'should determine 0! to equal 1' do
-    n = 0
-    expect(factorial(n)).to eq 1
+    expect(factorial(0)).to eq 1
   end
 end
