f1845bba2e4ce2864bccb6a3e210178db9a2992e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-10-03 20:45:11 -0700

    working through quick sort

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -1,14 +1,23 @@
-def quick_sort(array)
-  if array.length < 2
-    return array
-  else
-    pivot = array[-1]
-
+def quick_sort(array, start_index, end_index)
+  if start_index < end_index
+    partition_index = partition(array, start_index, end_index)
+    quick_sort(array, start_index, partition_index-1)
+    quick_sort(array, partition_index+1, end_index)
   end
+  return array
 end
 
-#
-def partition(array)
+# returns the index of the pivot in the array after rearrangement
+def partition(array, start_index, end_index)
+  pivot = array[end_index]
+  partition_index = start_index
+  for i in start_index..end_index
+    if array[i] <= pivot
+      swap(array, array[i], array[partition_index])
+      partition_index += 1
+    end
+  end
+  return partition_index
 end
 
 # swap the location of two items in the array
@@ -18,5 +27,3 @@ def swap(array, first_index, second_index)
 	array[second_index] = temp
   return array
 end
-
-# quick_sort([1,2,3])
--- spec/quick_sort_spec.rb
+++ spec/quick_sort_spec.rb
@@ -3,6 +3,7 @@ require 'quick_sort'
 RSpec.describe 'quick_sort' do
   it 'should return a sorted array' do
     array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
-    expect(quick_sort(array)).to eq [2, 3, 5, 6, 7, 9, 10, 11, 12, 14]
+    end_index = array.length-1
+    expect(quick_sort(array, 0, end_index)).to eq [2, 3, 5, 6, 7, 9, 10, 11, 12, 14]
   end
 end
